7cb5ccea9a06ceb381fb9269060c3b366273726d
Gabriel <zarloche@gmail.com>
2024-02-26 18:11:54 -0300

    feat: add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './Components/Header';
+import Home from './Components/Home';
 
 const App = () => {
-  return <div>App asdasasds React</div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <main className="appMain">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Header.module.css';
+
+const Header = () => {
+  return (
+    <>
+      <nav className={styles.nav}>
+        <div className={styles.navLink}>
+          <p>Gabriel Majeski</p>
+          <Link to="/">_home</Link>
+          <Link to="/">_sobre</Link>
+          <Link to="/">_projetos</Link>
+        </div>
+        <Link to="/">_contatos</Link>
+      </nav>
+    </>
+  );
+};
+
+export default Header;
