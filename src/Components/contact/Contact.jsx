2d073927fad6bfb4f4a30e92577463181557a9d6
Gabriel <zarloche@gmail.com>
2024-03-12 15:11:06 -0300

    feat: add contact page

--- src/Components/contact/Contact.jsx
+++ src/Components/contact/Contact.jsx
@@ -3,6 +3,8 @@ import styles from './Contact.module.css';
 import Dropdown from '../dropdown/Dropdown';
 import MailIcon from '../../assets/MailIcon';
 import WhatsIcon from '../../assets/WhatsappIcon';
+import useForm from '../../Hooks/useForm';
+import Input from '../form/Input';
 
 const opts = [
   {
@@ -18,26 +20,87 @@ const opts = [
 ];
 
 const Contact = () => {
+  const date = new Date();
+  const options = {
+    weekday: 'short',
+    month: 'short',
+    day: 'numeric',
+  };
+  const firstName = useForm('');
+  const email = useForm('email');
+  const [message, setMessage] = React.useState('');
+
+  console.log();
+
   return (
     <>
       <aside className={styles.aside}>
         <Dropdown data={opts} custom={false} text={'contatos'}></Dropdown>
       </aside>
-      <section className={styles.container}>
-        <form action="">
-          <label htmlFor="">_nome</label>
-          <input type="text" />
-          <label htmlFor="">_email</label>
-          <input type="text" />
-          <label htmlFor="">_mensagem</label>
-          <input type="text" />
-          <input type="button" value="" />
-        </form>
 
+      <form action="" className="animeLeft">
+        <Input
+          label={'_nome:'}
+          type="text"
+          name="firstName"
+          {...firstName}
+        ></Input>
+        <Input label={'_email:'} type="text" name="email" {...email}></Input>
         <div>
-          <h1>TEXTO</h1>
+          <label htmlFor="message">_mensagem:</label>
+          <textarea
+            id="message"
+            cols="30"
+            rows="10"
+            onChange={({ target }) => setMessage(target.value)}
+          ></textarea>
         </div>
-      </section>
+        <button>enviar</button>
+      </form>
+
+      <code
+        className={'animeRight'}
+        style={{ counterReset: 'line', color: '#4D5BCE' }}
+      >
+        <span data-line style={{ color: '#4D5BCE' }}>
+          <span style={{ color: '#E99287' }}>const </span>
+          button<span style={{ color: '#E99287' }}> = </span>
+          document.querySelector(
+          <span style={{ color: '#FEA55F' }}>'#sendBtn'</span>);
+        </span>
+        <span data-line>
+          <span style={{ color: '#E99287' }}>const </span> message
+          <span style={{ color: '#E99287' }}> = </span> &#123;
+        </span>
+        <span data-line>
+          <span> name:</span>{' '}
+          <span style={{ color: '#FEA55F' }}>"{firstName.value}"</span>,
+        </span>
+        <span data-line>
+          <span> email:</span>{' '}
+          <span style={{ color: '#FEA55F' }}>"{email.value}"</span>,
+        </span>
+        <span data-line>
+          <span> message:</span>{' '}
+          <span style={{ color: '#FEA55F' }}>"{message}"</span>,
+        </span>
+        <span data-line>
+          <span style={{ color: 'gray' }}> date: </span>
+          {}
+          <span style={{ color: '#FEA55F' }}>
+            "{date.toLocaleDateString('pt-BR', options).replace('.', '')}"
+          </span>
+        </span>
+        <span data-line>&#125;</span>
+        <span data-line> </span>
+        <span data-line>
+          button.addEventListener('
+          <span style={{ color: '#FEA55F' }}>'click'</span>'), ()
+          <span style={{ color: '#E99287' }}> =&#62;</span> &#123;
+        </span>
+        <span data-line> form.send(message);</span>
+        <span data-line>&#125;)</span>
+      </code>
     </>
   );
 };
