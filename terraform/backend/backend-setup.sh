affa82206ece8d34d05b48de99ba83534207d92b
gabrieruu <gabrieru.h@gmail.com>
2024-08-28 18:49:01 -0300

    test: fine tuning terraform pipeline

--- terraform/backend/backend-setup.sh
+++ terraform/backend/backend-setup.sh
@@ -3,19 +3,25 @@
 set -e
 
 ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
-TERRAFORM_BACKEND=$(GITHUB_WORKSPACE)/terraform/backend
+TERRAFORM_BACKEND=$GITHUB_WORKSPACE/terraform/backend
 BUCKET_NAME="terraform-state-${ACCOUNT_ID}"
 DYNAMODB_TABLE_NAME="terraform-lock-table-${ACCOUNT_ID}"
+
+echo "echoing script variables..."
+echo $ACCOUNT_ID
+echo $TERRAFORM_BACKEND
+echo $BUCKET_NAME
+echo $DYNAMODB_TABLE_NAME
 
 if ! aws s3api head-bucket --bucket "$BUCKET_NAME" 2>/dev/null; then
   echo "Backend S3 bucket does not exist. Creating..."
 
-  terraform init -chdir=$(TERRAFORM_BACKEND) -backend-config=backend_override.tf
-  terraform apply -chdir=$(TERRAFORM_BACKEND) -auto-approve
+  terraform init -chdir=$TERRAFORM_BACKEND -backend-config=backend_override.tf
+  terraform apply -chdir=$TERRAFORM_BACKEND -auto-approve
 else
   echo "Backend S3 bucket already exists. Continuing..."
 fi
 
 echo "Initializing S3 backend..."
-terraform init -chdir=$(TERRAFORM_BACKEND)
-terraform state push local.tfstate -chdir=$(TERRAFORM_BACKEND)
+terraform init -chdir=$TERRAFORM_BACKEND
+terraform state push local.tfstate -chdir=$TERRAFORM_BACKEND
