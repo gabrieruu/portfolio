b859e6484fea8fba17009730bc008348f1ec0a3f
gabrieruu <gabrieru.h@gmail.com>
2024-08-28 18:58:17 -0300

    test: fine tuning terraform pipeline

--- terraform/backend/backend-setup.sh
+++ terraform/backend/backend-setup.sh
@@ -10,12 +10,13 @@ DYNAMODB_TABLE_NAME="terraform-lock-table-${ACCOUNT_ID}"
 if ! aws s3api head-bucket --bucket "$BUCKET_NAME" 2>/dev/null; then
   echo "Backend S3 bucket does not exist. Creating..."
 
-  terraform init -chdir=$TERRAFORM_BACKEND -backend-config=backend_override.tf
-  terraform apply -chdir=$TERRAFORM_BACKEND -auto-approve
+  cd $TERRAFORM_BACKEND
+  terraform init -backend-config=backend_override.tf
+  terraform apply -auto-approve
 else
   echo "Backend S3 bucket already exists. Continuing..."
 fi
 
 echo "Initializing S3 backend..."
-terraform init -chdir=$TERRAFORM_BACKEND
-terraform state push local.tfstate -chdir=$TERRAFORM_BACKEND
+cd $TERRAFORM_BACKEND
+terraform init
