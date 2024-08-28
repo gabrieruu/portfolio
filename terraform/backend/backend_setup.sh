f44ca49f2981cd299ef74491265704dbbf4d6d20
gabrieruu <gabrieru.h@gmail.com>
2024-08-28 19:56:19 -0300

    test: fine tuning terraform pipeline

--- terraform/backend/backend_setup.sh
+++ terraform/backend/backend_setup.sh
@@ -11,7 +11,7 @@ if ! aws s3api head-bucket --bucket "$BUCKET_NAME" 2>/dev/null; then
   echo "Backend S3 bucket does not exist. Creating..."
 
   cd $TERRAFORM_BACKEND
-  terraform init -backend-config=backend_override.tf
+  terraform init -backend-config=$TERRAFORM_BACKEND/backend_override.tf
   terraform apply -auto-approve
 else
   echo "Backend S3 bucket already exists. Continuing..."
@@ -19,4 +19,4 @@ fi
 
 echo "Initializing S3 backend..."
 cd $TERRAFORM_BACKEND
-terraform init -backend-config=backend.tf -migrate-state -force-copy
+terraform init -backend-config=$TERRAFORM_BACKEND/backend.tf -migrate-state -force-copy
