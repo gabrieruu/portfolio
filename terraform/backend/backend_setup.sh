ddb24e8166d75674b994f2215c6897b5ab790637
gabrieruu <gabrieru.h@gmail.com>
2024-08-28 22:04:18 -0300

    test: fine tuning terraform pipeline

--- terraform/backend/backend_setup.sh
+++ terraform/backend/backend_setup.sh
@@ -7,7 +7,7 @@ TERRAFORM_BACKEND=$GITHUB_WORKSPACE/terraform/backend
 BUCKET_NAME="terraform-state-$ACCOUNT_ID"
 DYNAMODB_TABLE_NAME="terraform-lock-table-$ACCOUNT_ID"
 AWS_REGION="sa-east-1"
-STATE_KEY="backend.tfstate"
+TF_STATE_KEY="backend.tfstate"
 
 if ! aws s3api head-bucket --bucket "$BUCKET_NAME" 2>/dev/null; then
   echo "Backend S3 bucket does not exist. Creating..."
@@ -21,18 +21,11 @@ if ! aws s3api head-bucket --bucket "$BUCKET_NAME" 2>/dev/null; then
   echo "bucket         = ${BUCKET_NAME}" > config.s3.tfbackend
   echo "dynamodb_table = ${DYNAMODB_TABLE_NAME}" >> config.s3.tfbackend
   echo "region         = ${AWS_REGION}" >> config.s3.tfbackend
-  echo "key            = ${STATE_KEY}" >> config.s3.tfbackend
-  cat config.s3.tfbackend
-
+  echo "key            = ${TF_STATE_KEY}" >> config.s3.tfbackend
 else
   echo "Backend S3 bucket already exists. Continuing..."
-  echo "bucket         = ${BUCKET_NAME}" > config.s3.tfbackend
-  echo "dynamodb_table = ${DYNAMODB_TABLE_NAME}" >> config.s3.tfbackend
-  echo "region         = ${AWS_REGION}" >> config.s3.tfbackend
-  echo "key            = ${STATE_KEY}" >> config.s3.tfbackend
-  cat config.s3.tfbackend
 fi
 
-# echo "Initializing S3 backend..."
-# cd $TERRAFORM_BACKEND
-# terraform init -migrate-state -force-copy
+echo "Initializing S3 backend..."
+cd $TERRAFORM_BACKEND
+terraform init -backend-config=config.s3.tfbackend -migrate-state -force-copy
