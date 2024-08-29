334bc4ed05e40e998b0db4786039c4a77b7d50c4
gabrieruu <gabrieru.h@gmail.com>
2024-08-28 21:15:43 -0300

    test: fine tuning terraform pipeline

--- terraform/backend/dynamodb/terraform_lock_table.tf
+++ terraform/backend/dynamodb/terraform_lock_table.tf
@@ -1,3 +1,5 @@
+data "aws_caller_identity" "current" {}
+
 resource "aws_dynamodb_table" "terraform_lock" {
   name         = "terraform-lock-table-${data.aws_caller_identity.current.account_id}"
   billing_mode = "PAY_PER_REQUEST"
