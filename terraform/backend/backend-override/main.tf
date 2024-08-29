f2b52cd1456c3904bc0a60246ae758915012b4ad
gabrieruu <gabrieru.h@gmail.com>
2024-08-28 21:04:13 -0300

    test: fine tuning terraform pipeline

---
+++ terraform/backend/backend-override/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = "~> 1.8"
+}
+
+module "dynamodb" {
+  source = "../dynamodb"
+}
+
+module "s3" {
+  source = "../s3"
+}
--- terraform/backend/backend_setup.sh
+++ terraform/backend/backend_setup.sh
@@ -10,15 +10,11 @@ DYNAMODB_TABLE_NAME="terraform-lock-table-${ACCOUNT_ID}"
 if ! aws s3api head-bucket --bucket "$BUCKET_NAME" 2>/dev/null; then
   echo "Backend S3 bucket does not exist. Creating..."
 
-  cd $TERRAFORM_BACKEND
-  echo $(pwd)
-  terraform init -backend-config=$TERRAFORM_BACKEND/backend_override.tf
+  terraform init -chdir=$TERRAFORM_BACKEND/backend-override
   terraform apply -auto-approve
 else
   echo "Backend S3 bucket already exists. Continuing..."
 fi
 
 echo "Initializing S3 backend..."
-cd $TERRAFORM_BACKEND
-echo $(pwd)
-terraform init -backend-config=$TERRAFORM_BACKEND/backend.tf -migrate-state -force-copy
+terraform init -chdir=$TERRAFORM_BACKEND -migrate-state -force-copy
