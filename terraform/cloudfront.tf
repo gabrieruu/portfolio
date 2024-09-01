ff91e92617e73ba1c590ff9c5333dd69abdee9e3
gabrieruu <gabrieru.h@gmail.com>
2024-09-01 04:32:52 -0300

    ops: create website infrastructure

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -11,23 +11,23 @@ resource "aws_s3_bucket" "media" {
   }
 }
 
-resource "aws_s3_bucket_policy" "media_bucket_policy" {
-  bucket = aws_s3_bucket.media.id
+# resource "aws_s3_bucket_policy" "media_bucket_policy" {
+#   bucket = aws_s3_bucket.media.id
 
-  # policy = jsonencode({
-  #   Version = "2012-10-17",
-  #   Statement = [
-  #     {
-  #       Effect = "Allow",
-  #       Principal = {
-  #         AWS = aws_cloudfront_origin_access_identity.origin_identity.iam_arn
-  #       },
-  #       Action   = "s3:GetObject",
-  #       Resource = "${aws_s3_bucket.media.arn}/*"
-  #     }
-  #   ]
-  # })
-}
+#   # policy = jsonencode({
+#   #   Version = "2012-10-17",
+#   #   Statement = [
+#   #     {
+#   #       Effect = "Allow",
+#   #       Principal = {
+#   #         AWS = aws_cloudfront_origin_access_identity.origin_identity.iam_arn
+#   #       },
+#   #       Action   = "s3:GetObject",
+#   #       Resource = "${aws_s3_bucket.media.arn}/*"
+#   #     }
+#   #   ]
+#   # })
+# }
 
 resource "aws_cloudfront_distribution" "cdn" {
   origin {
