18ed9fb50afeb0caec77f7239675eb2554235f8e
gabrieruu <gabrieru.h@gmail.com>
2024-09-01 15:56:36 -0300

    ops: fine tuning infrastructure

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -41,7 +41,24 @@ resource "aws_cloudfront_distribution" "cdn" {
 
   enabled             = true
   is_ipv6_enabled     = true
-  default_root_object = "index.html"
+
+  default_cache_behavior {
+    target_origin_id       = "EC2-Origin"
+    viewer_protocol_policy = "redirect-to-https"
+    allowed_methods        = ["GET", "HEAD", "OPTIONS"]
+    cached_methods         = ["GET", "HEAD"]
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl     = 0
+    default_ttl = 3600
+    max_ttl     = 86400
+  }
 
   ordered_cache_behavior {
     path_pattern           = "/static/*"
