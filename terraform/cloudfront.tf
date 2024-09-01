748c92859e1f34373d3b2589cf8cdcbb8f28a658
gabrieruu <gabrieru.h@gmail.com>
2024-09-01 15:55:21 -0300

    ops: fine tuning infrastructure

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -39,39 +39,9 @@ resource "aws_cloudfront_distribution" "cdn" {
     }
   }
 
-  origin {
-    origin_id   = "EC2-Origin"
-    domain_name = var.domain_name
-
-    custom_origin_config {
-      http_port              = 80
-      https_port             = 443
-      origin_protocol_policy = "http-only"
-      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
-    }
-  }
-
   enabled             = true
   is_ipv6_enabled     = true
   default_root_object = "index.html"
-
-  default_cache_behavior {
-    target_origin_id       = "EC2-Origin"
-    viewer_protocol_policy = "redirect-to-https"
-    allowed_methods        = ["GET", "HEAD", "OPTIONS"]
-    cached_methods         = ["GET", "HEAD"]
-
-    forwarded_values {
-      query_string = false
-      cookies {
-        forward = "none"
-      }
-    }
-
-    min_ttl     = 0
-    default_ttl = 3600
-    max_ttl     = 86400
-  }
 
   ordered_cache_behavior {
     path_pattern           = "/static/*"
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,5 +1,10 @@
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = "AWSMainVPC"
+    Type = "VPC"
+  }
 }
 
 resource "aws_subnet" "public" {
@@ -7,6 +12,34 @@ resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = element(var.public_subnets, count.index)
   map_public_ip_on_launch = true
+
+  tags = {
+    Name = "AWSMainPublicSubnet-${count.index}"
+    Type = "Subnet"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "InternetGateway"
+    Type = "IGW"
+  }
+}
+
+data "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+  filter {
+    name   = "association.subnet-id"
+    values = aws_subnet.public[*].id
+  }
+}
+
+resource "aws_route" "internet_access" {
+  route_table_id         = data.aws_route_table.main.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
 }
 
 output "vpc_id" {
