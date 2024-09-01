98521be1badeacbdc2e73ee5906b7e0436e72257
gabrieruu <gabrieru.h@gmail.com>
2024-08-31 21:00:24 -0300

    ops: create website infrastructure

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "web" {
   ami                         = var.ami_id
   instance_type               = var.instance_type
-  subnet_id                   = aws_vpc.public_subnet_ids[0]
-  vpc_security_group_ids      = [aws_security_group.web_sg_id]
+  subnet_id                   = aws_subnet.public[0].id
+  vpc_security_group_ids      = [aws_security_group.web_sg.id]
   associate_public_ip_address = true
   key_name                    = var.key_name
 
