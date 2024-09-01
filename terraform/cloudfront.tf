3964fc22cb1f95dca4e90ed14e600e5281d09375
gabrieruu <gabrieru.h@gmail.com>
2024-08-31 21:03:57 -0300

    ops: create website infrastructure

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -51,6 +51,7 @@ resource "aws_cloudfront_distribution" "cdn" {
       http_port              = 80
       https_port             = 443
       origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
     }
   }
 
