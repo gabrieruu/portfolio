1877649dfc23e9926bcdf412e2dbe0e7695d36cf
gabrieruu <gabrieru.h@gmail.com>
2024-08-31 22:38:24 -0300

    ops: create website infrastructure

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 0.12"
+  backend "s3" {
+    bucket = "terraform-state-381491983236"
+    key    = "terraform.tfstate"
+    region = "sa-east-1"
+  }
+  
+}
+
+
 provider "aws" {
   region = var.aws_region
 }
