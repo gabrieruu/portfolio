000bd932d65538008f78402de3086a478ccfe831
gabrieruu <gabrieru.h@gmail.com>
2024-09-01 17:22:06 -0300

    ops: fine tuning infrastructure

---
+++ terraform/acm.tf
@@ -0,0 +1,39 @@
+resource "aws_acm_certificate" "certificate" {
+  domain_name       = var.domain_name
+  validation_method = "DNS"
+
+  tags = {
+    Name = "PortfolioSSLCertificate-sa-east-1"
+    Type = "ACMCertificate"
+  }
+}
+
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.certificate.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      type   = dvo.resource_record_type
+      record = dvo.resource_record_value
+    }
+  }
+
+  zone_id = var.route53_zone_id
+  name    = each.value.name
+  type    = each.value.type
+  ttl     = 60
+  records = [each.value.record]
+
+  lifecycle {
+    create_before_destroy = true
+    ignore_changes = [records]
+  }
+}
+
+resource "aws_acm_certificate_validation" "cert_validation" {
+  certificate_arn         = aws_acm_certificate.certificate.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
+
+output "certificate_arn" {
+  value = aws_acm_certificate.certificate.arn
+}
---
+++ terraform/alb.tf
@@ -0,0 +1,88 @@
+resource "aws_lb" "web_lb" {
+  name               = "webserver-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.web_sg.id]
+
+  subnets = [
+    aws_subnet.public[0].id,
+    aws_subnet.public[1].id
+  ]
+
+  enable_deletion_protection = false
+
+  tags = {
+    Name = "PortfolioWebServerALB"
+    Type = "ALB"
+  }
+
+  depends_on = [
+    aws_security_group.web_sg,
+    aws_subnet.public
+  ]
+}
+
+resource "aws_lb_target_group" "web_tg" {
+  name        = "webserver-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
+  target_type = "instance"
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+  }
+
+  depends_on = [
+    aws_vpc.main
+  ]
+}
+
+resource "aws_lb_target_group_attachment" "web_tg_attachment" {
+  target_group_arn = aws_lb_target_group.web_tg.arn
+  target_id        = aws_instance.web.id
+  port             = 80
+
+  depends_on = [
+    aws_instance.web
+  ]
+}
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.web_lb.arn
+  port              = 443
+  protocol          = "HTTPS"
+
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = aws_acm_certificate.certificate.arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web_tg.arn
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.web_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+output "alb_dns_name" {
+  value = aws_lb.web_lb.dns_name
+}
